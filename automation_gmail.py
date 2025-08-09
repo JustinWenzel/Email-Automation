eb07b23fe6d4d04a99ec406892109c25ec485b47
JustinWenzel <157823610+JustinWenzel@users.noreply.github.com>
2025-08-09 16:58:36 +0200

    automation_gmail.py

--- automation_gmail.py
+++ automation_gmail.py
@@ -6,9 +6,12 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 
+
+#Berichtsheft reminder for private and work mail
 load_dotenv()
 
 EMAIL = os.getenv("EMAIL_ADDRESS")
+WORK_EMAIL = os.getenv("EMAIL_ADDRESS_WORK")
 PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 # IMAP_SERVER = "imap.gmail.com" later project
@@ -19,19 +22,20 @@ def loadBerichtsheftReminder():
     message = MIMEText("Schreibe dein Berichtsheft!")
     message["Subject"] = "Berichtsheft"
     message["From"] = EMAIL
-    message["To"] = EMAIL
+    message["To"] = f"{EMAIL},{WORK_EMAIL}"
 
     SMTP_SERVER = "smtp.gmail.com"
     with smtplib.SMTP_SSL(SMTP_SERVER, 465) as smtp:
         smtp.login(EMAIL, PASSWORD)
         smtp.send_message(message)
 
-#schedule.every(1).minutes.do(loadBerichtsheftReminder) for testing purposes
+#schedule.every(1).minutes.do(loadBerichtsheftReminder) #for testing purposes
 schedule.every().friday.at("16:30").do(loadBerichtsheftReminder)
+
+print("Script läuft!")
 
 while True:
     schedule.run_pending()
     time.sleep(1)
 
-print("Script läuft!")
 
